5972abed0332ebbef8bf03fe7f36a3f7be785fb2
Alexander Li <alexander.li@ef.com>
2016-10-11 13:22:35 +0800

    initialize watch and listener function

--- libs/step1/controller/steps.js
+++ libs/step1/controller/steps.js
@@ -2,20 +2,48 @@ angular.module('stepOne.controller.steps', [])
     .controller('steps', [
         '$scope',
         function ($scope, $elem){
+            var imagePrefix = './resources/img/'
             
             function Scope(){
+                this.$$watchers = [];
+            }
 
+            Scope.prototype.$watch = function (watchFunc, listenerFunc){
+                var watcher = {
+                    watchFunc: watchFunc,
+                    listenerFunc: listenerFunc
+                };
+
+                this.$$watchers.push(watcher);
+            }
+
+            Scope.prototype.$digest = function (){
+                this.$$watchers.forEach(function (watcher, index, self){
+                    watcher.listenerFunc();
+                });
             }
 
             $scope.visualArr = [
                 {
                     title: 'Start by creating Scope Class',
-                    content: 'Angular scopes are plain old JavaScript objects, on which you can attach properties just like you would on any other object. Scopes are created using the Scope constructor. Let\'s make the simplest possible version of it:',
-                    url: './resources/img/createScopeClass.png'
+                    content: 'Angular scopes are plain old JavaScript objects, \
+                                on which you can attach properties just like you \
+                                would on any other object. Scopes are created using \
+                                the Scope constructor. \
+                                <br />Let\'s make the simplest possible version of it:',
+                    url: imagePrefix + 'createScopeClass.jpeg'
                 },
                 {
-                    content: 'test2',
-                    url: 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/logo_white_fe6da1ec.png'
+                    title: 'Watching Object Properties: $watch And $digest',
+                    content: '$watch and $digest are two sides of the same coin. \
+                                Together they form the core of what Angular scopes are all about: \
+                                Reacting to changes in data. \
+                                With $watch you can attach a watcher to a scope.<br /> \
+                                A watcher is something that is notified when a change occurs in the scope. \
+                                You create a watcher by providing two functions to $watch:<br /> \
+                                A watch function, which specifies the piece of data you\'re interested in.<br />\
+                                A listener function which will be called whenever that data changes.',
+                    url: imagePrefix + 'watchAndListener.jpeg'
                 }
             ];
         }
--- libs/step1/directive/step.js
+++ libs/step1/directive/step.js
@@ -1,27 +1,31 @@
 angular.module('stepOne.directive.step', [])
-    .directive('step', function (){
-        return {
-            restrict: 'E',
-            replace: true,
-            // transclude: true,
-            template: '<div class="blog-post">'
-                        + '<h3 style="cursor: pointer;">{{title}}<small ng-show="footnote">{{footnote}}</small></h3>'
-                        + '<img ng-show="show" class="thumbnail" ng-src="{{screencapture}}" />'
-                        + '<p ng-show="show">{{content}}</p>'
-                    + '</div>',
-            scope: {
-                'title': '=',
-                'screencapture': '=',
-                'content': '=',
-                'show': '=',
-                'footnote': '='
-            },
-            link: function ($scope, $elem, $attr){
-                $elem = angular.element($elem); // convert into jQuery element for DOM manipulation convenience.
-                $elem.on('click', 'h3', function (ev){
-                    $scope.show = !$scope.show;
-                    $scope.$digest();
-                });
+    .directive('step', [
+        '$sce',
+        function ($sce) {
+            return {
+                restrict: 'E',
+                replace: true,
+                // transclude: true,
+                template: '<div class="blog-post">'
+                + '<h3 style="cursor: pointer;"><small ng-show="footnote">{{footnote}}</small>.{{title}}</h3>'
+                + '<img ng-show="show" class="thumbnail" ng-src="{{screencapture}}" />'
+                + '<p ng-show="show" ng-bind-html="content"></p>'
+                + '</div>',
+                scope: {
+                    'title': '=',
+                    'screencapture': '=',
+                    'content': '=',
+                    'show': '=',
+                    'footnote': '='
+                },
+                link: function ($scope, $elem, $attr) {
+                    $elem = angular.element($elem); // convert into jQuery element for DOM manipulation convenience.
+                    $scope.content = $sce.trustAsHtml($scope.content);
+                    $elem.on('click', 'h3', function (ev) {
+                        $scope.show = !$scope.show;
+                        $scope.$digest();
+                    });
+                }
             }
         }
-    });
+    ]);
